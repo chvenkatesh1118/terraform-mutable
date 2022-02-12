d44c62639ac86914e1c955f2e6427155683bc0c2
Chanti <chvenkatesh.1118@gmail.com>
2022-02-12 13:56:37 +0530

    25-11

--- vpc/env/s3.tf
+++ vpc/env/s3.tf
@@ -1,12 +1,14 @@
+
+provider "aws"  {
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket" "chantich" {
   bucket = "my-tf-test-bucket"
 
   tags = {
-    Name        = "chantich"
+    Name  = "chantich"
 
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
