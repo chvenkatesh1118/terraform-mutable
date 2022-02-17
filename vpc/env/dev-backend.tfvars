5b1d65646c10535159b45cb8e0e7bc8c560c849c
Chanti <chvenkatesh.1118@gmail.com>
2022-02-17 11:11:06 +0530

    11-02

--- vpc/env/dev-backend.tfvars
+++ vpc/env/dev-backend.tfvars
@@ -1,3 +1,3 @@
-bucket =  "chantich"
+bucket =  "chanti.ch"
 key = "terraform-mutable/vpc/dev/terraform.tfstate"
 region = "us-east-1"
--- vpc/subnets.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.robo.id
-  cidr_block = var.CIDR_BLOCK
-  aws_availability_zone = var.AZS
-
-  tags = {
-    Name = var.ENV
-  }
-}
