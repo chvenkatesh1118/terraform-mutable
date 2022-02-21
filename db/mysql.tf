95ac06541f89f99d0b85e3765a9e01e23065cb07
Chanti <chvenkatesh.1118@gmail.com>
2022-02-21 15:36:10 +0530

    11-02

--- db/mysql.tf
+++ db/mysql.tf
@@ -3,9 +3,24 @@ resource "aws_db_instance" "chantisql" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "var.ENV-db"
+  name                 = "my-db"
   username             = "admin"
   password             = "chanti@123"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+}
+
+resource "aws_db_parameter_group" "pg" {
+  name   = "rds-pg"
+  family = "mysql5.6"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8"
+  }
+
+
+}
+provider "aws" {
+  region = "us-east-1"
 }
